ac63817e1a53532012bb176c6c340d8af701e4a9
Jorge Rodriguez <j.a.rodriguez.lopez@accenture.com>
2016-06-16 17:13:33 -0600

    added passport and secured some routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var app = express();
 
@@ -14,26 +17,32 @@ var nav = [{
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 var port = 8181;
 
 app.use(express.static('public'));
-app.set('views', './src/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'library'
+}));
 
+require('./src/config/passport')(app);
+
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
-        nav: [{
-            link: '/Books',
-            text: 'Books'
-        }, {
-            link: '/Authors',
-            text: 'Authors'
-        }]
+        nav: nav
     });
 });
 
